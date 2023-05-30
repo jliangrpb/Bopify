83a6c362a427068c61b47b59ad7faa9fb4baf2cb
jliangrpb <jeremy.liang612@gmail.com>
2023-05-30 11:28:06 -0400

    proj setup

---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ApplicationRecord
+  has_secure_password validations: false
+  before_validation :ensure_session_token
+
+  validates :session_token, uniqueness: true, presence: true
+
+  def self.find_by_credentials(credential, password)
+    if URI::MailTo::EMAIL_REGEXP.match(credential)
+      user = User.find_by(email: credential)
+    else
+      user = User.find_by(username: credential)
+    end
+
+    if user
+      return user if user.authenticate(password)
+    else
+      return nil
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = generate_unique_session_token
+    self.save!
+    self.session_token
+  end
+  
+  private
+
+  def generate_unique_session_token
+    token = SecureRandom.urlsafe_base64
+    while User.exists?(session_token: token)
+      token = SecureRandom.urlsafe_base64
+    end
+    return token
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+  
+end
