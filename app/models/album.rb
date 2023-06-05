0a81658745eed8e58c4965b09a2bbbb34d278d3c
jliangrpb <jeremy.liang612@gmail.com>
2023-06-05 09:27:55 -0400

    Added AWS setup to the project

--- app/models/album.rb
+++ app/models/album.rb
@@ -17,8 +17,9 @@ class Album < ApplicationRecord
         foreign_key: :artist_id,
         class_name: :Artist
 
-    has_many :songs,
-        primary_key: :id,
-        foreign_key: :album_id,
-        
+    # has_many :songs,
+    #     primary_key: :id,
+    #     foreign_key: :album_id,
+
+
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,6 +9,8 @@
 #
 class Artist < ApplicationRecord
     validates :name, presence: true 
+    
+    has_one_attached :photo
 
     has_many :albums,
         primary_key: :id,
